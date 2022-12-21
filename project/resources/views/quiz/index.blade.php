98512c3c6d132097e62ef24433d9bd2dee06ecac
新島　駿 <niijima131229@docomo.ne.jp>
2022-12-21 14:14:52 +0900

    quizテーブル、モデルの作成

--- project/resources/views/quiz/index.blade.php
+++ project/resources/views/quiz/index.blade.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
-    <style>
-    </style>
     <title>東京地名クイズ</title>
 </head>
 
